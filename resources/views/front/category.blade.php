3e4be0d0fbd4705e805fec608f7194df01766a53
smile-zhao <prayer_zhao@outlook.com>
2016-03-31 22:44:26 +0200

    update css

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -4,14 +4,16 @@
         <div class="row">
             <div class="article-layout-flex">
                 @foreach($articles as $article)
-                    <div class="article-item col-xs-12 col-sm-6 col-md-4 col-lg-3">
-                        <div class="article_image responsive-image">
-                            <img src="{{$article->page_image}}" alt="{{$article->title}}" class="img-responsive">
-                        </div>
-                        <div class="article_content">
-                            <h3 class="article_title">{{$article->title}}</h3>
-                            <p class="article_intro">{{$article->intro}}</p>
-                        </div>
+                    <div class="article-list col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                        <article class="article-item">
+                            <div class="article-image responsive-image">
+                                <img src="{{$article->page_image}}" alt="{{$article->title}}" class="img-responsive">
+                            </div>
+                            <div class="article-text">
+                                <h3 class="article-title">{{$article->title}}</h3>
+                                <p class="article-intro">{{$article->intro}}</p>
+                            </div>
+                        </article>
                     </div>
                 @endforeach
             </div>

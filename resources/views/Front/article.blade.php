28a7a5f82879eee5fa2809030a4061e1e2eb6220
smile-zhao <prayer_zhao@outlook.com>
2016-04-02 23:44:28 +0200

    update article page

--- resources/views/Front/article.blade.php
+++ resources/views/Front/article.blade.php
@@ -2,14 +2,16 @@
 @section('content')
     <div class="news-content">
         <div class="row">
-            <article>
+            <article class="article-body">
                 <h1>{{$article->title}}</h1>
-                <ul>
-                    <li>{{$article->user->name}}</li>
-                    <li>{{$article->published_at}}</li>
-                </ul>
-                <p></p>
-                <p>{{$article->intro}}</p>
+                <div class="article-info">
+                    <ul class="list-inline">
+                        <li class="article-author">{{$article->user->name}}</li>
+                        <li>{{$article->publish_date}}</li>
+                        <li>{{$article->publish_time}}</li>
+                    </ul>
+                    <p>{{$article->intro}}</p>
+                </div>
                 <div>
                     {{$article->content}}
                 </div>
